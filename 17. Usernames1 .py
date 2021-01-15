a12cec887c9d2ef35d4e706edcba034ef5bb8093
Sam Kaspar <samuelckaspar@gmail.com>
2021-01-14 17:20:02 -0800

    my second  changes

--- 17. Usernames1 .py
+++ 17. Usernames1 .py
@@ -1,2 +1,2 @@
-a= input("What is your google email(ex. alfred@google.com)?: ")
+a= input("What is your google email(ex. alfredchris@google.com)?: ")
 print("Your username is "+a[:-11])
--- 18. Usernames2 .py
+++ 18. Usernames2 .py
@@ -1,2 +1,2 @@
 a=input("What is your google username without the google stuff(ex. jeffery)?: ")
-print("Your email is "+a+"@google.com")
+print("Your email is "+a+"@fie.com")
