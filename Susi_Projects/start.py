03c95e07610b40fb8ab50d1ee50486c11595a33d
sckjesus <samuelckaspar@gmail.com>
2022-06-29 05:31:41 -0500

    changes

--- Susi_Projects/start.py
+++ Susi_Projects/start.py
@@ -1,6 +1,7 @@
 
-def say(thing):
+def say(thing) :
     print(thing)
+
 
 name=input("give me a name ")
 say("hi "+name)
--- projects/7.  Yes or No .py
+++ projects/7.  Yes or No .py
@@ -1,6 +1,14 @@
 for i in range(20):
-    a=input("Say something, if you say yes, then I will say Yes, but if you say something else, I will say No: ")
-    if a.lower()=="yes":
-        print("Yes")
+    a=input("who's cool?: ")
+    if "sam" in a.lower() or "son" in a.lower():
+        print("Yes, he's actually pretty cool")
+    elif 'susi' in a.lower() or 'susa' in a.lower():
+        print("I guess she's kinda okay...")
+    elif 'grace' in a.lower():
+        print("okay. chris is weird enough.. but grace! i won't even say anything.s")
+    elif a.lower()=='chris':
+        print("NoNoNoNo!n he's the worst daddy pig ever!!!")
+    elif a.lower()=='christopher':
+        print("No. even that name is weird. he farts so big too")
     else:
-        print("No")
+        print('who is that? that weirdo')
